0341a583ea4b67b19d865dd933dbb1081ab031fc
Victor Rubio <vctrubio@gmail.com>
2020-09-27 23:14:02 +0200

    OK hving some trouble to deploy to heroku for test

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,10 +1,35 @@
 class FormsController < ApplicationController
 
     def index
-        @forms = Form.all    
+        @forms = Form.all  
+        @form = Form.new  
     end
 
-    
+    def create
+      @form = Form.new(form_params)
+      if @form.cinco == true
+        @form.total = 5
+      end
+      if
+        @form.diez == true
+        @form.total = 10
+      end
+      if
+        @form.diez && @form.cinco == true
+        @form.total = 15
+      end
+      respond_to do |format|
+        if @form.save
+          format.html { redirect_to root_path, notice: 'Form was successfully created.' }
+          format.json { render :show, status: :created, location: root_path }
+        else
+          format.html { render :new }
+          format.json { render json: @form.errors, status: :unprocessable_entity }
+        end
+      end
+
+    end
+
     private
     # Use callbacks to share common setup or constraints between actions.
     def set_form
@@ -13,7 +38,7 @@ class FormsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def form_params
-      params.require(:form).permit(:name, :phone, :address, :five, :ten, :total, :delivered)
+      params.require(:form).permit(:nombre, :tlf, :direccion, :cinco, :diez, :total, :delivered, :comments)
     end
 
     def destroy
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "starts"
+Form.create(nombre: "Mrs Canabis", tlf: "616 696 999", direccion: "Camino Ancho 214", cinco: true, total: 5 )
+Form.create(nombre: "Maria de la Lora", tlf: "616 690 790", direccion: "Camino Ancho 100", cinco: true, total: 5 )
+Form.create(nombre: "Hash Maron", tlf: "629 690 969", direccion: "Camino Guachupino 7", diez: true, total: 10 )
+Form.create(nombre: "Younes", tlf: "629 690 000", direccion: "Avenida del Jefe 1", diez: true, cinco: true, total: 5 )
+Form.create(nombre: "Presidente Perez", tlf: "600 100 000", direccion: "Avenida Junta de Andalucia 29", diez: true, cinco: true, total: 15 )
+puts "done"
