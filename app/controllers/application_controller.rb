c31dd626f26af41dd0147742fd850f871d137e97
Victor Rubio <vctrubio@gmail.com>
2020-09-27 23:48:39 +0200

    lets try this

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!, except: [:index, :show, :create]
-  skip_before_action :authenticate_user!, raise: false
+  # skip_before_action :authenticate_user!, raise: false NOPE
 
   # skip_before_action :authenticate_user!
 end
